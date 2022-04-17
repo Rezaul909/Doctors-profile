59f8edc6ecd9a56af31bfb677cbd04c40081e9ea
Rezaul909 <reza1381996@gmail.com>
2022-04-17 14:04:03 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footer-container mt-5 text-white'>
             <h1>This is footer</h1>
+            <p>All right reserves to @2022</p>
         </div>
     );
 };
